cc8d0c1b3fe06982513d034da93157090b08d8a2
Ivan <ivanhallcloninger@gmail.com>
2019-09-16 15:38:53 -0700

    fix app.py

--- app.py
+++ app.py
@@ -16,13 +16,14 @@ def index():
     params = {
         'q': query_term,
         'key': "BAI80UHWOVOB"
+        'limit': '10'
     }
     # TODO: Make an API call to Tenor using the 'requests' library. For
     # reference on how to use Tenor, see:
     # https://tenor.com/gifapi/documentation
     # TODO: Use the '.json()' function to get the JSON of the returned response
     # object
-    r = requests.get("https://api.tenor.com/v1/search?", params =  params)
+    r = requests.get("https://api.tenor.com/v1/search", params =  params)
     data = r.json()
     # if r.status_code == 200:
     #     # load the GIFs using the urls for the smaller GIF sizes
@@ -33,12 +34,16 @@ def index():
 
     # TODO: Using dictionary notation, get the 'results' field of the JSON,
     # which contains the GIFs as a list
-    results = data["results"][0:10]
+    results = data["results"]
+    # [0:100]
 
     # TODO: Render the 'index.html' template, passing the list of gifs as a
     # named parameter called 'gifs'
 
-    return render_template("index.html", results=results)
+    gifs = render_template('index.html',
+    query_term = query_term,
+    results=results)
 
+    return gifs
 if __name__ == '__main__':
     app.run(debug=True)
