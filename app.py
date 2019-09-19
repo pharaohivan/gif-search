49b80cd9f3abe2cd4ee5eed3df59ee63c21a2b2d
Ivan <ivanhallcloninger@gmail.com>
2019-09-19 16:14:43 -0700

    added a fix and new read me

--- app.py
+++ app.py
@@ -7,37 +7,33 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """Return homepage."""
-    # TODO: Extract the query term from url using request.args.get()
+    #  Extracted the query term from url using request.args.get()
     query_term = request.args.get('q')
-    # TODO: Make 'params' dictionary containing:
-    # a) the query term, 'q'
-    # b) your API key, 'key'
-    # c) how many GIFs to return, 'limit'
+    """
+    Made a 'params' dictionary containing:
+    a) the query term, 'q'
+    b) my API key, 'key'
+    c) how many GIFs to return, 'limit'
+    """
     params = {
         'q': query_term,
         'key': "BAI80UHWOVOB",
         'limit': '12'
     }
-    # TODO: Make an API call to Tenor using the 'requests' library. For
-    # reference on how to use Tenor, see:
-    # https://tenor.com/gifapi/documentation
-    # TODO: Use the '.json()' function to get the JSON of the returned response
-    # object
-    r = requests.get("https://api.tenor.com/v1/search", params =  params)
-    data = r.json()
-    # if r.status_code == 200:
-    #     # load the GIFs using the urls for the smaller GIF sizes
-    #     top_8gifs = json.loads(r.content)
-    #     print top_8gifs
-    # else:
-    #     top_8gifs = None
 
-    # TODO: Using dictionary notation, get the 'results' field of the JSON,
+    """Made an API call to Tenor using the 'requests' library"""
+    r = requests.get("https://api.tenor.com/v1/search", params =  params)
+
+    #  Used the '.json()' function to get the JSON of the returned response
+    # object
+    data = r.json()
+
+    # Using dictionary notation, we get the 'results' field of the JSON,
     # which contains the GIFs as a list
     results = data["results"]
     # [0:100]
 
-    # TODO: Render the 'index.html' template, passing the list of gifs as a
+    # We rendered the 'index.html' template, passing the list of gifs as a
     # named parameter called 'gifs'
 
     gifs = render_template('index.html',
---
+++ newREADME.md
@@ -0,0 +1,2 @@
+Hello, Welcome to our gif search! We created this web app using flask and we hope you enjoy what we created.
+To find any gif you are looking for simply just type in the name of whatever it is you want and click submit or press enter on your keyboard and it will give you all the gifs that are related to your search query.
